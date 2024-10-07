bec713979910af2d5af374e167e748fc86999b98
contactjual <contactjual@gmail.com>
2024-10-08 00:18:50 +0600

    plm solved

--- tasks/task3-plm.js
+++ tasks/task3-plm.js
@@ -1,3 +1,21 @@
 
 // Write a function to count the number of vowels in a string.
 
+function containsAllVowels(input) {
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    input = input.toLowerCase();
+    let count = 0;
+
+    for (const key of input) {
+        if (vowels.includes(key)) {
+            count++;
+        }
+    }
+
+    return count;
+}
+const input = 'aaaeesdfgh';
+console.log(containsAllVowels(input));
+
+
+
